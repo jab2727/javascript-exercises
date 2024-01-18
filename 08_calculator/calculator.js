42055a90dcfa1edfb7b0fc46c5e42bf27b148df4
Jeff Borack <jborack@gmail.com>
2024-01-18 15:13:53 -0500

    Completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let results = 0;
+  for (let number in array){
+    results += array[number];
+  }
+  return results;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let results = 1;
+  for (let number in array){
+    results *= array[number];
+  }
+  return results;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a==0) {
+    return 1;
+  }
+  let result = a;
+  while (a>1) {
+    result *= (a-1)
+    a--
+  }
+  return result;
 };
 
 // Do not edit below this line
