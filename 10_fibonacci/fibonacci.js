a77f94a9d071f1b5c2b5fea13b4b0eea925274c4
Jeff Borack <jborack@gmail.com>
2024-01-19 11:19:48 -0500

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(iterations) {
+    let firstNumber = 1;
+    let secondNumber = 0;
+    if (Number(iterations)<0){
+        return "OOPS"
+    }
+    for (i=0;i<Number(iterations); i++){
+        solution = firstNumber + secondNumber;
+        secondNumber = firstNumber;
+        firstNumber = solution;
+        console.log(secondNumber);
+    }
+    return secondNumber;
 };
+
+console.log(fibonacci(0));
 
 // Do not edit below this line
 module.exports = fibonacci;
