475c9ea1e167655f3c86bdd7a0cbf5914cbd4efb
Jeff Borack <jborack@gmail.com>
2024-01-18 22:00:01 -0500

    Completed exercise 9.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (isPalindrome) {
+    isPalindrome = isPalindrome.replace(/\W/g, '').toLowerCase();
+    let answer = true;
+    console.log(isPalindrome.length);
+    for (let i=0; i<isPalindrome.length; i++){
+        console.log(isPalindrome[i]);
+        console.log(isPalindrome[isPalindrome.length-1-i]);
+        if (!(isPalindrome[i]===isPalindrome[isPalindrome.length-1-i])){
+            answer = false;
+            console.log("Answer falsified.")
+        }
+    }
+    return answer;
 };
+
+console.log(palindromes("racecar"));
 
 // Do not edit below this line
 module.exports = palindromes;
