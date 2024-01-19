d34748c5d8d4f7a3c55c0b26ed71169c06231980
Jeff Borack <jborack@gmail.com>
2024-01-19 12:54:37 -0500

    Completed exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,20 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(list) {
+    let names = list.map(item => item.title);
+    return(names);
 };
+
+// const books = [
+//     {
+//       title: 'Book',
+//       author: 'Name'
+//     },
+//     {
+//       title: 'Book2',
+//       author: 'Name2'
+//     }
+//   ]
+
+// console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
