7c60cf7a4a57fca103978b2f97547b7970294af5
Jeff Borack <jborack@gmail.com>
2024-02-13 10:00:16 -0500

    Tic Tac Toe working, but not as a factory function

---
+++ scope/scope.js
@@ -0,0 +1,10 @@
+function outer(){
+    const outerVar = "Hey I am the outer var";
+    return function inner(){
+        const innerVar = 'Hey I am an inner var';
+        console.log(innerVar);
+        console.log(outerVar);
+    }
+}
+
+const innerFn = outer();
